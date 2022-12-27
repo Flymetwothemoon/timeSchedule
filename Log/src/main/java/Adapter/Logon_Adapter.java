c3329efcce74db34008be025685cf5990d740ad3
Flymetwothemoon <2113203787@qq.com>
2022-12-27 16:53:46 +0800

    day 7

---
+++ Log/src/main/java/Adapter/Logon_Adapter.java
@@ -0,0 +1,29 @@
+package Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class Logon_Adapter extends FragmentStateAdapter {
+    List<Fragment>mFragments;
+
+    public Logon_Adapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle,List<Fragment>fragments) {
+        super(fragmentManager, lifecycle);
+        mFragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFragments.size();
+    }
+}
