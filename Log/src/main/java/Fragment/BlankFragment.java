c02b9e7f49c5a54912f853bb2dbfadd68f856cbe
Flymetwothemoon <2113203787@qq.com>
2022-12-29 18:53:11 +0800

    day 8

--- Log/src/main/java/Fragment/BlankFragment.java
+++ Log/src/main/java/Fragment/BlankFragment.java
@@ -7,31 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
-import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.log.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Logon_Fragment#newInstance} factory method to
+ * Use the {@link BlankFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-@Route(path = "/logon/logon1")
-public class Logon_Fragment extends Fragment {
+public class BlankFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private View view;
-    private EditText number;
-    private EditText password;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public Logon_Fragment() {
+    public BlankFragment() {
         // Required empty public constructor
     }
 
@@ -41,11 +36,11 @@ public class Logon_Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Logon_Fragment.
+     * @return A new instance of fragment BlankFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Logon_Fragment newInstance(String param1, String param2) {
-        Logon_Fragment fragment = new Logon_Fragment();
+    public static BlankFragment newInstance(String param1, String param2) {
+        BlankFragment fragment = new BlankFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,14 +61,6 @@ public class Logon_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        if(view==null) {
-            view = inflater.inflate(R.layout.fragment_logon_, container, false);
-        }
-        init();
-        return view;
-    }
-    private void init(){
-        number = view.findViewById(R.id.number_edit);
-        password = view.findViewById(R.id.password_edit1);
+        return inflater.inflate(R.layout.fragment_blank, container, false);
     }
 }
--- Log/src/main/java/Fragment/Forget_Fragment.java
+++ Log/src/main/java/Fragment/Forget_Fragment.java
@@ -2,53 +2,36 @@ package Fragment;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.log.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Log_Fragment#newInstance} factory method to
+ * Use the {@link Forget_Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-@Route(path = "/log2/log3")
-public class Log_Fragment extends Fragment implements View.OnClickListener {
+@Route(path = "/logon/logon1")
+public class Forget_Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private View view;
+    private EditText number;
+    private EditText password;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private EditText account;
-    private EditText password;
-    private Button log;
 
-
-    Fragment current;
-
-    private TextView email;
-    private TextView logon;
-
-    private TextView forget;
-    public Log_Fragment() {
+    public Forget_Fragment() {
         // Required empty public constructor
     }
 
@@ -58,11 +41,11 @@ public class Log_Fragment extends Fragment implements View.OnClickListener {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Log_Fragment.
+     * @return A new instance of fragment Forget_Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Log_Fragment newInstance(String param1, String param2) {
-        Log_Fragment fragment = new Log_Fragment();
+    public static Forget_Fragment newInstance(String param1, String param2) {
+        Forget_Fragment fragment = new Forget_Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -79,34 +62,18 @@ public class Log_Fragment extends Fragment implements View.OnClickListener {
         }
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         if(view==null) {
-           view =  inflater.inflate(R.layout.fragment_log_, container, false);
+            view = inflater.inflate(R.layout.fragment_forget_, container, false);
         }
-         current = getChildFragmentManager().findFragmentById(R.id.logactivity);
         init();
-
-        log.setOnClickListener(this);
-
         return view;
     }
     private void init(){
-        account = view.findViewById(R.id.Account_edit);
-        password = view.findViewById(R.id.password_edit);
-        log = view.findViewById(R.id.Log_button);
-
+        number = view.findViewById(R.id.number_edit);
+        password = view.findViewById(R.id.password_edit1);
     }
-
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.Log_button){
-            ARouter.getInstance().build("/main/main1").navigation();
-        }
-    }
-    }
+}
