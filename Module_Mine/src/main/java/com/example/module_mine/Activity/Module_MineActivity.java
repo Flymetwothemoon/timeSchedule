cec130e719b5aacb2cb4ea5204f01f5b07bcf17f
Flymetwothemoon <2113203787@qq.com>
2023-01-06 23:12:15 +0800

    day 14

--- Module_Mine/src/main/java/com/example/module_mine/Activity/Module_MineActivity.java
+++ Module_Mine/src/main/java/com/example/module_mine/Activity/Module_MineActivity.java
@@ -1,13 +1,24 @@
 package com.example.module_mine.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.example.module_mine.R;
+import com.example.module_mine.fragment.achievementFragment;
+import com.example.module_mine.fragment.competitionFragment;
+import com.example.module_mine.fragment.friendFragment;
+import com.example.module_mine.fragment.manageFragment;
+import com.example.module_mine.fragment.myactivityFragment;
+import com.example.module_mine.fragment.nameFragment;
+import com.example.module_mine.fragment.pictureFragment;
+import com.example.module_mine.fragment.sumFragment;
 
 public class Module_MineActivity extends AppCompatActivity {
 
@@ -21,5 +32,35 @@ public class Module_MineActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         //透明导航栏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        skip();
+    }
+    private void skip(){
+        String id = getIntent().getStringExtra("TAG");
+        Log.d("TAG2",id);
+        if(id.equals("circle")){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new pictureFragment()).commit();
+        }
+        if(id.equals("name")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new nameFragment()).commit();
+        }
+        if(id.equals("award")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new achievementFragment()).commit();
+        }
+        if(id.equals("competition")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new competitionFragment()).commit();
+        }
+        if(id.equals("friend")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new friendFragment()).commit();
+        }
+        if(id.equals("sum")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new sumFragment()).commit();
+        }
+        if(id.equals("activity")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new myactivityFragment()).commit();
+        }
+        if(id.equals("manage")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity,new manageFragment()).commit();
+        }
     }
 }
