bba9bfd5aee0aa2fe55b397c6392c04d3bef72ed
Flymetwothemoon <2113203787@qq.com>
2023-01-07 23:55:33 +0800

    day 15

--- Module_Mine/src/main/java/com/example/module_mine/Activity/Rule_Activity.java
+++ Module_Mine/src/main/java/com/example/module_mine/Activity/Rule_Activity.java
@@ -3,14 +3,16 @@ package com.example.module_mine.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.example.module_mine.R;
 
 public class Rule_Activity extends AppCompatActivity {
-
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +23,22 @@ public class Rule_Activity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         //透明导航栏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        init();
+    }
+    private void init(){
+        mTextView = findViewById(R.id.textview);
+        writeTextView();
+        changeTextViewStyle();
+    }
+
+    private void changeTextViewStyle() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"TsangerYuYangT_W02_W02.ttf");
+        mTextView.setTypeface(typeface);
+    }
+
+    private void writeTextView(){
+        mTextView.setText("当累计步数在0-40000之间时为铜牌\n当累计步数在40000-120000之间时为银牌\n" +
+                "当累计步数超过120000时为金牌\n每次我的成就会选择优先级最高的\n" +
+                "获奖优先级:金牌>银牌>铜牌");
     }
 }
