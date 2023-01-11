61d4992a60c2946fbcc63607e5be9b0500efb02e
Flymetwothemoon <2113203787@qq.com>
2023-01-11 10:27:58 +0800

    day 19

--- Module_Mine/src/main/java/com/example/module_mine/fragment/pictureFragment.java
+++ Module_Mine/src/main/java/com/example/module_mine/fragment/pictureFragment.java
@@ -1,8 +1,15 @@
 package com.example.module_mine.fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,14 +18,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.module_mine.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,6 +47,7 @@ public class pictureFragment extends Fragment implements View.OnClickListener {
     private String mParam1;
     private String mParam2;
     private View view;
+    private CircleImageView picture_image;
     public pictureFragment() {
         // Required empty public constructor
     }
@@ -80,6 +92,7 @@ public class pictureFragment extends Fragment implements View.OnClickListener {
     private void init(){
         Button button_0 = view.findViewById(R.id.open_album);
         Button button_1 = view.findViewById(R.id.alum_save);
+        picture_image = view.findViewById(R.id.picture_image);
         button_0.setOnClickListener(this);
         button_1.setOnClickListener(this);
     }
@@ -110,6 +123,7 @@ public class pictureFragment extends Fragment implements View.OnClickListener {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //后面的操作是把挑选结果返回给activity
+        handleImageOnKitKat(data);
     }
 
     /**
@@ -123,4 +137,55 @@ public class pictureFragment extends Fragment implements View.OnClickListener {
         startActivityForResult(albumIntent, ALBUM_RESULT_CODE);
         //这一步是为了下一步的回调
     }
+    @TargetApi(value = 19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(getActivity(), uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];
+                // 解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content: //downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是content类型的Uri，则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是file类型的Uri，直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        // 根据图片路径显示图片
+        displayImage(imagePath);
+        System.out.println(imagePath);
+    }
+
+    /**获取图片的路径*/
+    @SuppressLint("Range")
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getActivity().getContentResolver().query(uri, null, selection, null, null);
+        if(cursor != null){
+
+            if(cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+
+    /**展示图片*/
+    private void displayImage(String imagePath) {
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+        picture_image.setImageBitmap(bitmap);
+    }
+
+
 }
